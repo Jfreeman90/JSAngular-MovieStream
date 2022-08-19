f29f54af4cdf5c09b87b2494da8c5acf5cf4ae3e
jackf <jack.freeman1990@gmail.com>
2022-08-19 09:40:57 +0100

    added font awesome module and icons to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+//import the icons you want to use from the package
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faTurnUp} from '@fortawesome/free-solid-svg-icons';
+import { faHome} from '@fortawesome/free-solid-svg-icons';
+import { faPen} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login-signup';
+  faCoffee = faCoffee;
+  faTurnUp = faTurnUp;
+  faHome = faHome;
+  faPen=faPen;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyMaterialModule } from  './material.module';
+import { RouterModule, Routes } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MyMaterialModule, 
+    FontAwesomeModule,
+    RouterModule.forRoot([
+      {path: '', redirectTo: '/', pathMatch: 'full'},
+      {path: 'register', component: RegistrationComponent},
+      {path: 'login', component: LoginComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]

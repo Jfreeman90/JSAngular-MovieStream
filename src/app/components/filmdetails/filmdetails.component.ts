e5562770962a5fe3a0504312664d7391da9cdb4a
jackf <jack.freeman1990@gmail.com>
2022-08-19 15:28:38 +0100

    Film detail component added and styled

--- src/app/components/filmdetails/filmdetails.component.ts
+++ src/app/components/filmdetails/filmdetails.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-filmdetails',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./filmdetails.component.css']
 })
 export class FilmdetailsComponent implements OnInit {
+  //star icon
+  faStar=faStar;
 
   constructor() { }
 

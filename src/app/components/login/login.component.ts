af8bc012b8572dc94bcca7ae6ed7b8b5867fdbcc
jackf <jack.freeman1990@gmail.com>
2022-08-22 21:18:11 +0100

    Username component linked to database

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/User';                  //user interface to get the user json format
+import { UserService } from 'src/app/user.service';   //user service to get in touch with the API
 
 @Component({
   selector: 'app-login',
@@ -9,9 +11,10 @@ export class LoginComponent implements OnInit {
   //set up the varables with the same name as the forms
   username: string;
   password: string;
+  //variable for the user element which links to the user interface which holds all information that is returned
+  user: User;
 
-  constructor(
-  ) { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
   }
@@ -26,15 +29,19 @@ export class LoginComponent implements OnInit {
       alert('Please enter a password!');
       return
     }
-    
-    //create the new object
-    const newLogin={
-      username: this.username,
-      password: this.password
-    }
 
-    //print the form values
-    console.log(newLogin)
+    //look up the user based on username
+    //take the email the user input and go through the database to find the username and confirm an email has been sent.
+    this.userService.getUserFromUsername(this.username).subscribe((user) =>{
+      this.user=user;
+      //check for empty response 'status:500' which means there is no user with this username.
+
+      //check that the password entered matches the password in the database.
+      if (this.password===this.user.password){
+        console.log("Log in succesful")
+      }
+    } );
+
 
     //clear the form after submission
     this.username='';
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -18,10 +18,18 @@ const httpOptions = {
 export class UserService {
   //from the sakila api look up the email from the users SQL table entered into the input file
   private apiURLEmailLookup:string='http://sakillaapi-env.eba-wnfxdqg3.us-east-1.elasticbeanstalk.com/users/email?email=';
+  private apiURLUsernameLookup:string='http://sakillaapi-env.eba-wnfxdqg3.us-east-1.elasticbeanstalk.com/users/user?username=';
   private apiURLAddUser:string='http://sakillaapi-env.eba-wnfxdqg3.us-east-1.elasticbeanstalk.com/users/add?email=';
 
   //constructor allows the use of get/post/patch/del
   constructor(private http:HttpClient) { }
+
+
+  //get user from username
+  getUserFromUsername(username: string): Observable<User>{
+    //return the task array from the api
+    return this.http.get<User>(this.apiURLUsernameLookup+username);
+  }
 
   //get user from email
   getUserFromEmail(email: string): Observable<User>{
@@ -33,6 +41,5 @@ export class UserService {
   addUser(email:string, username:string, password:string): Observable<User>{
     //add the user by using the user inteface
     return this.http.post<User>(this.apiURLAddUser+email+'&username='+username+'&password='+password, httpOptions)
-
   }
 }
